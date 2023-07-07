ef328d7c6fcc9161f2a0a58eba1c7157870d1013
rcsousa <ricardo.c.sousa@gmail.com>
2023-07-07 02:11:53 -0300

    final cleanup

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 import os
 import requests
-import subprocess
 import openai
 
 
 def main():
+    """
+    The main function of the program. This function reads a file, sends its content to OpenAI's GPT-35-TURBO API to generate an explanation, and writes the explanation to a README.md file. The README.md file will have a project description, instructions on how to install the dependencies and how to execute the code, how to contribute, and any references to external sources of information if available.
+    """
 
     openai.api_type = "azure"
     openai.api_version = "2023-05-15" 
-    openai.api_base = os.getenv("OPENAI_API_BASE")  # Your Azure OpenAI resource's endpoint value.
+    openai.api_base = os.getenv("OPENAI_API_BASE") 
     openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-    #get current directory
-    #current_dir = os.getcwd()
 
     # Get the filename from the environment variables
     filename = os.environ['FILENAME']
@@ -23,7 +22,7 @@ def main():
         code = file.read()
 
     response = openai.ChatCompletion.create(
-        engine="gpt-35-turbo-16k", # The deployment name you chose when you deployed the GPT-35-Turbo or GPT-4 model.
+        engine="gpt-35-turbo-16k", 
         temperature=0,
         messages=[
             {"role": "system", "content": "You are a GitHub Actions automation bot that upon exectution, will create a README.md file with the explanation of the code in this repository."},
@@ -31,12 +30,7 @@ def main():
         ]
     )
 
-    print(response)
-
     explanation = response['choices'][0]['message']['content']
-
-    print(explanation)
-
 
     # Write the explanation to the README.md file
     with open('README.md', 'w') as file:
